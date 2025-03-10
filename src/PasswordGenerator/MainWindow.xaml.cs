96d6ef141c1e46c057ad79905e040dcd20397b85
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2025-03-10 19:51:53 +0100

    Initial commit

---
+++ src/PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Domain;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+			var label = new Class1()
+			{
+				Password = "123456Test"
+			};
+
+			LabelToSee.Content = label.Password;
+		}
+    }
+}
---
+++ src/Shared/Domain/Class1.cs
@@ -0,0 +1,9 @@
+
+namespace Domain
+{
+	public class Class1
+	{
+		public string Password { get; set; }
+	}
+
+}
