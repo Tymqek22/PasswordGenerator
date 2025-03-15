57d939c0af9b599a2aa8972ea979dc7e93337159
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2025-03-15 18:10:18 +0100

    Password Model added

--- src/Shared/Domain/Password.cs
+++ src/Shared/Domain/Password.cs
@@ -1,3 +1,4 @@
+using System.Text;
 
 namespace Domain
 {
@@ -8,7 +9,43 @@ namespace Domain
 		public bool UseLowerCase { get; set; }
 		public bool UseDigits { get; set; }
 		public bool UseSpecialCharacters { get; set; }
-		public string GeneratedPassword { get; set; }
+		public string? GeneratedPassword { get; set; }
+
+		public void GeneratePassword()
+		{
+			StringBuilder generatedPassword = new();
+			Random rand = new();
+
+			string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			string lowercase = "abcdefghijklmnopqrstuvwxyz";
+			string special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}";
+			string digits = "0123456789";
+
+			StringBuilder charactersBase = new();
+
+			for (int i = 0; i < 5; i++) {
+
+				if (this.UseUpperCase) {
+					charactersBase.Append(uppercase[rand.Next(uppercase.Length)]);
+				}
+				if (this.UseLowerCase) {
+					charactersBase.Append(lowercase[rand.Next(lowercase.Length)]);
+				}
+				if (this.UseDigits) {
+					charactersBase.Append(digits[rand.Next(digits.Length)]);
+				}
+				if (this.UseSpecialCharacters) {
+					charactersBase.Append(special[rand.Next(special.Length)]);
+				}
+			}
+
+			for (int i = 0; i < this.Length; i++) {
+
+				generatedPassword.Append(charactersBase[rand.Next(charactersBase.Length)]);
+			}
+
+			this.GeneratedPassword = generatedPassword.ToString();
+		}
 	}
 
 }
